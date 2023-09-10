f9c60b1ee6d63d47f6b02a7927c706c9798fceb5
datnt <datngotien345@gmail.com>
2023-09-10 12:35:34 +0700

    update recipe show feature

--- src/main/java/guru/springframework/services/RecipeServiceImp.java
+++ src/main/java/guru/springframework/services/RecipeServiceImp.java
@@ -5,6 +5,7 @@ import guru.springframework.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,5 +21,15 @@ public class RecipeServiceImp implements RecipeService{
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if(recipeOptional.isPresent()){
+            return recipeOptional.get();
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.services.RecipeService;
+import guru.springframework.services.RecipeServiceImp;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class RecipeControllerTest extends TestCase {
+    // RecipeServiceImp recipeService;
+    @Mock
+    RecipeService recipeService;
+    RecipeController controller;
+    @Before
+    public void setUp() throws Exception {
+        //super.setUp();
+        MockitoAnnotations.initMocks(this);
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetDetailRecipe() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
